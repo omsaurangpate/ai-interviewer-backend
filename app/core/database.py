db38fe078c732052e861440f76b6433ec5ee3a12
Om Saurangpate <omsaurangpate07@gmail.com>
2025-05-14 00:37:14 +0530

    database connection

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,7 @@
 import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 from app.core.config import settings
 
